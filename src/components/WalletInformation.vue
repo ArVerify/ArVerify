1ee3fca7361070dff11ddcbfc6aa23f89995c9f1
fabianriewe <f.riewe@riewe-software.de>
2020-09-23 22:26:19 +0200

    added arweave integration

--- src/components/WalletInformation.vue
+++ src/components/WalletInformation.vue
@@ -2,14 +2,14 @@
   <div class="flex flex-col justify-center">
     <div class="flex justify-center">
       <div class="self-center px-6 font-bold text-3xl">{</div>
-      <div class="self-center font-light">address</div>
+      <div class="self-center font-light">{{ address }}</div>
       <div class="self-center px-6 font-bold text-3xl">}</div>
     </div>
     <h-bar/>
     <div class="px-48">
       <div class="text-center font-bold text-lg">Your Balance.</div>
       <div class="bg-gray-500 py-5">
-        <div class="text-center text-white font-light text-2xl">0.01020102102012 AR</div>
+        <div class="text-center text-white font-light text-2xl">{{balance}} AR</div>
       </div>
     </div>
   </div>
@@ -17,10 +17,21 @@
 
 <script>
     import HBar from "./HBar";
+    import {getBalance, getCurrentAddress} from "../plugins/vue-arweave";
 
     export default {
         name: "WalletInformation",
-        components: {HBar}
+        components: {HBar},
+        data() {
+            return {
+                address: undefined,
+                balance: undefined
+            }
+        },
+        async mounted() {
+            this.address = await getCurrentAddress()
+            this.balance = await getBalance()
+        }
     }
 </script>
 
---
+++ src/plugins/vue-arweave.js
@@ -0,0 +1,23 @@
+import Arweave from 'arweave';
+
+// Since v1.5.1 you're now able to call the init function for the web version without options. The current path will be used by default, recommended.
+const $ar = Arweave.init();
+
+export async function getCurrentAddress() {
+    let key = JSON.parse(localStorage.getItem("jwk"))
+    if (key) {
+        return await $ar.wallets.jwkToAddress(key)
+    }
+    else return undefined
+}
+
+export async function getBalance() {
+    let address = await getCurrentAddress()
+    if (address) {
+        let balance = await $ar.wallets.getBalance(address)
+        return $ar.ar.winstonToAr(balance)
+    }
+    else return undefined
+}
+
+export default $ar
