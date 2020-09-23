5d218a401acfb587fc58025c4a15d18428b22109
fabianriewe <f.riewe@riewe-software.de>
2020-09-23 21:03:14 +0200

    added some padding

--- src/components/StatusCircle.vue
+++ src/components/StatusCircle.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div class="border border-dashed border-black rounded-full p-2">
+    <div class="border border-dashed border-black rounded-full p-4">
       <div class="border-20 border-black rounded-full">
         <div class="h-48 w-48">
-          <div>
-            Click here to verify
+          <div class="h-full flex p-2">
+            <div class="w-full self-center text-center text-3xl">Click here to verify</div>
           </div>
         </div>
       </div>
