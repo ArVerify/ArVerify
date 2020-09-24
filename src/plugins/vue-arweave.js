2c3cf3071b7157ebc37b272cd83bf0ae0314188f
fabianriewe <f.riewe@riewe-software.de>
2020-09-24 10:08:18 +0200

    refactored login

--- src/plugins/vue-arweave.js
+++ src/plugins/vue-arweave.js
@@ -3,6 +3,10 @@ import Arweave from 'arweave';
 // Since v1.5.1 you're now able to call the init function for the web version without options. The current path will be used by default, recommended.
 const $ar = Arweave.init();
 
+export function loggedIn() {
+    return !!localStorage.getItem('jwk')
+}
+
 export async function getCurrentAddress() {
     let key = JSON.parse(localStorage.getItem("jwk"))
     if (key) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="p-4">
-    <login></login>
     <navigation></navigation>
-    <wallet-information></wallet-information>
-    <h-bar></h-bar>
-    <status></status>
-    <h-bar></h-bar>
+    <div v-if="!loggedIn">
+      <login></login>
+    </div>
+    <div v-else>
+      <wallet-information></wallet-information>
+      <h-bar></h-bar>
+      <status></status>
+      <h-bar></h-bar>
+    </div>
   </div>
 </template>
 
 <script>
+    import {loggedIn} from '../plugins/vue-arweave';
     import Navigation from "../components/Navigation";
     import WalletInformation from "../components/WalletInformation";
     import Status from "../components/Status";
     import HBar from "../components/HBar";
     import Login from "../components/Login";
 
+
     export default {
         name: "Home",
-        components: {Login, HBar, Status, WalletInformation, Navigation}
+        components: {Login, HBar, Status, WalletInformation, Navigation},
+        data(){
+            return{
+                loggedIn: loggedIn()
+            }
+        }
     }
 </script>
 
